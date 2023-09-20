a206ed1b60e3f37175c76f747b0fea4d753667ec
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2023-09-20 17:48:16 -0600

    Update Tables_ventas.sql

--- Ventas/Tables_ventas.sql
+++ Ventas/Tables_ventas.sql
@@ -1,1 +1,9 @@
-
+-- Tabla de Clientes
+CREATE TABLE Clientes (
+    ClienteID VARCHAR2(30)PRIMARY KEY,
+    NombreCliente VARCHAR2(55),
+    PrimerApellidoCliente VARCHAR2(55),
+    SegundoApellidoCliente VARCHAR2(55),
+    NumeroTelefono VARCHAR2(20),
+    EmailCliente VARCHAR2(30)
+);
