9e35f399909398710eac925b51aa5d0ac4ca085a
Mario Cordero Ulate <91304423+Macordero2021@users.noreply.github.com>
2023-10-07 23:30:44 -0600

    Cambio de nombre de tablas por prefijo de RRHH y se agrega FK en tabla de Historial laboral

--- Modulos/RRHH/Tablas_Rrhh.sql
+++ Modulos/RRHH/Tablas_Rrhh.sql
@@ -1,20 +1,27 @@
-create table Departamentos
+
+----Inicio Modulo de RRHH----
+
+
+----Tabla Departamentos----
+create table RRHH_Departamentos
 (departamentoID varchar2(5),
 nombreDepartamento varchar2(50)
 );
 
-alter table Departamentos
+alter table RRHH_Departamentos
 add constraint pk_departamentoID primary key(departamentoID);
 
-create table Puestos
+----Tabla Puestos de Trabajo----
+create table RRHH_Puestos
 (puestoID varchar2(5),
 nombrePuesto varchar2(50)
 );
 
-alter table Puestos
+alter table RRHH_Puestos
 add constraint pk_puestoID primary key(puestoID);
 
-create table Empleados
+----Tabla de Empleados----
+create table RRHH_Empleados
 (empleadoID varchar2(5),
 nombreEmpleado varchar2(50),
 apellidoEmpleado varchar2(50),
@@ -24,23 +31,24 @@ telefono varchar2(20),
 email varchar2(20),
 departamentoID varchar2(5),
 puestoID varchar2(5),
-fechaIngreso date)
+fechaIngreso date
 );
 
-  alter table Empleados
+  alter table RRHH_Empleados
   add constraint pk_empleadoID primary key(empleadoID)
 
-  ALTER TABLE Empleados ADD (
+  ALTER TABLE RRHH_EmpleadosADD (
   CONSTRAINT FK_empleadoID
   FOREIGN KEY (departamentoID) 
-  REFERENCES Departamentos (departamentoID));
+  REFERENCES RRHH_Departamentos (departamentoID));
 
-  ALTER TABLE Empleados ADD (
+  ALTER TABLE RRHH_Empleados ADD (
   CONSTRAINT FK_puestoID
-  FOREIGN KEY (puestoID 
-  REFERENCES Puestos (puestoID));
+  FOREIGN KEY (puestoID)
+  REFERENCES RRHH_Puestos (puestoID));
 
-create table Contratos
+----Tabla de Contratos----
+create table RRHH_Contratos
 (contratoID varchar2(5),
 empleadoID varchar2(5),
 tipoContrato varchar2(5),
@@ -49,15 +57,16 @@ fechaFinContrato date,
 descripcionContrato varchar2(150)
 );
 
-alter table Contratos
+alter table RRHH_Contratos
 add constraint pk_contratoID primary key(contratoID)
 
-  ALTER TABLE Contratos ADD (
+  ALTER TABLE RRHH_Contratos ADD (
   CONSTRAINT FK_empleadoID2
   FOREIGN KEY (empleadoID) 
-  REFERENCES Empleados (empleadoID));
+  REFERENCES RRHH_Empleados (empleadoID));
 
-create table Salarios
+----Tabla de Salarios----
+create table RRHH_Salarios
 (salarioID varchar2(5),
 empleadoID varchar2(5),
 montoSalario varchar2(15),
@@ -66,15 +75,16 @@ fechaFinSalario date,
 descripcionSalario varchar2(150)
 );
 
-alter table Salarios
+alter table RRHH_Salarios
 add constraint pk_salarioID primary key(salarioID)
 
-  ALTER TABLE Salarios ADD (
+  ALTER TABLE RRHH_Salarios ADD (
   CONSTRAINT FK_empleadoID3
   FOREIGN KEY (empleadoID) 
-  REFERENCES Empleados (empleadoID));
+  REFERENCES RRHH_Empleados (empleadoID));
 
-create table Beneficios
+----Tabla de Beneficios----
+create table RRHH_Beneficios
 (beneficiosID varchar2(5),
 empleadoID varchar2(5),
 tipoBeneficio varchar2(15),
@@ -83,15 +93,16 @@ fechaInicioBeneficio date
 fechaFinBeneficio date
 );
 
-alter table Beneficios
+alter table RRHH_Beneficios
 add constraint pk_beneficiosID primary key(beneficiosID);
 
-  ALTER TABLE Beneficios ADD (
+  ALTER TABLE RRHh_Beneficios ADD (
   CONSTRAINT FK_empleadoID4
   FOREIGN KEY (empleadoID) 
-  REFERENCES Empleados (empleadoID));
+  REFERENCES RRHH_Empleados (empleadoID));
 
-create table Evaluaciones
+----Tabla de Evaluaciones----
+create table RRHH_Evaluaciones
 (evaluacionID varchar2(5),
 empleadoID varchar2(5)
 fechaEvaluacion date,
@@ -99,15 +110,16 @@ resultadoEvaluacion varchar2(20),
 comentarios varchar2(150)
 );
 
-alter table Evaluaciones
+alter table RRHH_Evaluaciones
 add constraint pk_evaluacionesID primary key(evaluacionID)
 
-  ALTER TABLE Evaluaciones ADD (
+  ALTER TABLE RRHH_Evaluaciones ADD (
   CONSTRAINT FK_empleadoID5
   FOREIGN KEY (empleadoID) 
-  REFERENCES Empleados (empleadoID));
+  REFERENCES RRHH_Empleados (empleadoID));
 
-create table Vacaciones
+----Tabla de Vacaciones----
+create table RRHH_Vacaciones
 (vacacionesID varchar2(5),
 empleadoID varchar2(5),
 fechaInicioVacaciones date,
@@ -115,15 +127,16 @@ fechaFinalVacaciones date,
 estadoVacaciones varchar2(10)
 );
 
-alter table Vacaciones
+alter table RRHH_Vacaciones
 add constraint pk_vacacionesID primary key(vacacionesID);
 
-  ALTER TABLE Vacaciones ADD (
+  ALTER TABLE RRHH_Vacaciones ADD (
   CONSTRAINT FK_empleadoID6
   FOREIGN KEY (empleadoID) 
-  REFERENCES Empleados (empleadoID));
+  REFERENCES RRHH_Empleados (empleadoID));
 
-create table Ausencias
+----Tabla de Ausencias----
+create table RRHH_Ausencias
 (ausenciasID varchar2(5),
 empleadoID varchar2(5),
 fechaInicioAusencia date,
@@ -131,15 +144,16 @@ fechaFinalAusencia date,
 tipoAusencia varchar2(10)
 );
 
-alter table Ausencias
+alter table RRHH_Ausencias
 add constraint pk_ausenciasID primary key(ausenciasID);
 
-  ALTER TABLE Ausencias ADD (
+  ALTER TABLE RRHH_Ausencias ADD (
   CONSTRAINT FK_empleadoID7
   FOREIGN KEY (empleadoID) 
-  REFERENCES Empleados (empleadoID));
+  REFERENCES RRHH_Empleados (empleadoID));
 
-create table Capacitaciones
+----Tabla de Capacitaciones----
+create table RRHH_Capacitaciones
 (capacitacionID varchar2(5),
 empleadoID varchar2(5),
 nombreCapacitacion varchar2(50),
@@ -148,18 +162,24 @@ fechaFinalCapacitacion date,
 descripcionCapacitacion varchar2(200)
 );
 
-alter table Capacitaciones
+alter table RRHH_Capacitaciones
 add constraint pk_capacitacionesID primary key(capacitacionID);
 
-  ALTER TABLE Capacitaciones ADD (
+  ALTER TABLE RRHH_Capacitaciones ADD (
   CONSTRAINT FK_empleadoID8
   FOREIGN KEY (empleadoID) 
-  REFERENCES Empleados (empleadoID));
+  REFERENCES RRHH_Empleados (empleadoID));
 
-create table HistorialLaboral
+----Tabla de Historial Laboral del empleado----
+create table RRHH_HistorialLaboral
 (historialID varchar2(5),
 empleadoID varchar2(5),
 fechaInicioTrabajo date,
 fechaFinalTrabajo date,
 comentariosTrabajo varchar2(100)
 );
+
+  ALTER TABLE RRHH_HistorialLaboral ADD (
+  CONSTRAINT FK_empleadoID9
+  FOREIGN KEY (empleadoID) 
+  REFERENCES RRHH_Empleados (empleadoID));
