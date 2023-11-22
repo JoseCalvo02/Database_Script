40db90be89d990f74ae27cf748d990ae586a53b1
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2023-11-22 17:05:45 -0600

    Update triggers.sql

--- Modulos/Ventas/Triggers/triggers.sql
+++ Modulos/Ventas/Triggers/triggers.sql
@@ -1,1 +1,16 @@
-
+--> Trigger #12 para la Tabla de Historial de Transferencias entre almacenes
+CREATE OR REPLACE TRIGGER trg_ven_bitacora
+AFTER INSERT OR UPDATE OR DELETE ON VEN_Bitacora
+FOR EACH ROW
+BEGIN
+    IF INSERTING THEN
+        INSERT INTO VEN_Bitacora (fechaHora, usuario, tipoMovimiento, entidadAfectada)
+        VALUES (SYSTIMESTAMP, USER, 'INSERT', 'INV_TransferenciasAlmacenes');
+    ELSIF UPDATING THEN
+        INSERT INTO VEN_Bitacora (fechaHora, usuario, tipoMovimiento, entidadAfectada)
+        VALUES (SYSTIMESTAMP, USER, 'UPDATE', 'INV_TransferenciasAlmacenes');
+    ELSIF DELETING THEN
+        INSERT INTO VEN_Bitacora (fechaHora, usuario, tipoMovimiento, entidadAfectada)
+        VALUES (SYSTIMESTAMP, USER, 'DELETE', 'INV_TransferenciasAlmacenes');
+    END IF;
+END;
