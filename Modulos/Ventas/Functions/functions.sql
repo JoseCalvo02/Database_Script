6506d387e9138bfff91161b8bedb7d57265c9873
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2023-11-22 12:16:01 -0600

    Update functions.sql

--- Modulos/Ventas/Functions/functions.sql
+++ Modulos/Ventas/Functions/functions.sql
@@ -1,1 +1,12 @@
+CREATE OR REPLACE FUNCTION obtener_ubicacion_envio(ubicacion_id VARCHAR2)
+
+RETURN DATE
+
+AS
+    fecha_envio DATE;
+BEGIN
+    SELECT fechaEnvio INTO fecha_envio FROM VEN_Envios WHERE ubicacionID = ubicacion_id;
+    RETURN NVL(fecha_envio,'NOT FOUND');
+    
+END;
 
