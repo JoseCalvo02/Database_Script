800ac1c93ff77deea52f48f4cc41d37d841478c0
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2023-11-22 17:10:47 -0600

    Update views.sql

--- Modulos/Ventas/Views/views.sql
+++ Modulos/Ventas/Views/views.sql
@@ -9,3 +9,22 @@ GROUP BY c.clienteID, c.nombreCliente;
 
 
 
+CREATE MATERIALIZED VIEW MV_TotalProductosPorPedido
+BUILD DEFERRED
+REFRESH COMPLETE ON DEMAND
+AS
+SELECT pedidoID, COUNT(productoID) AS totalProductos
+FROM VEN_Pedidos_productos
+GROUP BY pedidoID;
+
+CREATE VIEW VW_DetallesPedidosProductos AS
+SELECT p.pedidoID, p.fechaPedido, pp.productoID, pp.cantidad
+FROM VEN_Pedidos p
+JOIN VEN_Pedidos_productos pp ON p.pedidoID = pp.pedidoID;
+
+CREATE VIEW VW_ClientesConPedidos AS
+SELECT c.clienteID, c.nombreCliente, p.pedidoID, p.fechaPedido
+FROM VEN_Clientes c
+LEFT JOIN VEN_Pedidos p ON c.clienteID = p.clienteID;
+
+
