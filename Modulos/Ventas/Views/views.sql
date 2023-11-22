ac5ad6308e2766095cc0c3bdc6bd50d9115c188a
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2023-11-22 17:09:29 -0600

    Update views.sql

--- Modulos/Ventas/Views/views.sql
+++ Modulos/Ventas/Views/views.sql
@@ -1,1 +1,11 @@
+CREATE MATERIALIZED VIEW MV_PedidosPorCliente
+BUILD IMMEDIATE
+REFRESH FAST ON COMMIT
+AS
+SELECT c.clienteID, c.nombreCliente, COUNT(p.pedidoID) AS totalPedidos
+FROM VEN_Clientes c
+LEFT JOIN VEN_Pedidos p ON c.clienteID = p.clienteID
+GROUP BY c.clienteID, c.nombreCliente;
+
+
 
