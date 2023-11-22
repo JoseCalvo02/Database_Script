d8c515b4d3a886ac9ce9bbf1f8e6fe7c3b79ed45
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2023-11-22 16:21:54 -0600

    Grants de ventas corregidos

--- Modulos/Ventas/Role_Grants/Role_Grants.sql
+++ Modulos/Ventas/Role_Grants/Role_Grants.sql
@@ -2,15 +2,19 @@
 CREATE ROLE ventas_role;
 
 -- Otorgar grant con privilegios sobre las tablas ===> CORREGIR CON SUS DATOS
-GRANT ALL PRIVILEGES ON INV_ALERTASSTOCK TO inventarios_role;
-GRANT ALL PRIVILEGES ON INV_ALMACENES TO inventarios_role;
-GRANT ALL PRIVILEGES ON INV_CATEGORIAS TO inventarios_role;
-GRANT ALL PRIVILEGES ON INV_DEVOLUCIONES TO inventarios_role;
-GRANT ALL PRIVILEGES ON INV_GARANTIAS TO inventarios_role;
-GRANT ALL PRIVILEGES ON INV_HISTORIALMOVIMIENTOS TO inventarios_role;
-GRANT ALL PRIVILEGES ON INV_MARCAS TO inventarios_role;
-GRANT ALL PRIVILEGES ON INV_PRODUCTOS TO inventarios_role;
-GRANT ALL PRIVILEGES ON INV_PRODUCTOSPROVEEDORES TO inventarios_role;
-GRANT ALL PRIVILEGES ON INV_STOCK TO inventarios_role;
-GRANT ALL PRIVILEGES ON INV_TRANSFERENCIASALMACENES TO inventarios_role;
-GRANT ALL PRIVILEGES ON INV_UBICACIONES TO inventarios_role;
+GRANT ALL PRIVILEGES ON VEN_Clientes TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Pedidos TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Pedidos_productos TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Vendedores TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Encabezado_factura TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Detalle_factura TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Descuentos TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Descuentos_Productos TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Historial_Ventas TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Metodo_pago TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Factura_metodo_pago TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Envios TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Envios_detalle TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Promociones TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Devoluciones TO ventas_role;
+GRANT ALL PRIVILEGES ON VEN_Calificaciones_Clientes TO ventas_role;
