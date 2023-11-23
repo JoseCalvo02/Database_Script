7323e14cb606845ee7792e2e907ef6ffa864ebd6
Mario Cordero Ulate <91304423+Macordero2021@users.noreply.github.com>
2023-11-22 20:05:28 -0600

    .

--- Modulos/RRHH_Mario/Procedimientos/Procedimiento_RRHH.sql
+++ Modulos/RRHH_Mario/Procedimientos/Procedimiento_RRHH.sql
@@ -16,19 +16,3 @@ BEGIN
     );
 END;
 
--- "GetEmployeeDetails", toma una identificación de empleado como entrada y devuelve el nombre del empleado, el nombre del departamento, --
--- el nombre del puesto y el salario como parámetros de salida. Utiliza una instrucción SELECT INTO para recuperar los datos de las tablas relevantes.--
-
-DECLARE
-    v_employeeName VARCHAR2(50);
-    v_departmentName VARCHAR2(50);
-    v_positionName VARCHAR2(50);
-    v_salary VARCHAR2(15);
-BEGIN
-    GetEmployeeDetails('EMP001', v_employeeName, v_departmentName, v_positionName, v_salary);
-    DBMS_OUTPUT.PUT_LINE('Employee Name: ' || v_employeeName);
-    DBMS_OUTPUT.PUT_LINE('Department: ' || v_departmentName);
-    DBMS_OUTPUT.PUT_LINE('Position: ' || v_positionName);
-    DBMS_OUTPUT.PUT_LINE('Salary: ' || v_salary);
-END;
-
