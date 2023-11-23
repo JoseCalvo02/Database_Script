cd069488c0b1ea45805e86229328169f361bef36
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2023-11-23 10:40:59 -0600

    Update registers.sql

--- Modulos/Registers/registers.sql
+++ Modulos/Registers/registers.sql
@@ -2,19 +2,15 @@
 
 -- Inserting records into INV_Categorias table
 INSERT INTO INV_Categorias (categoriaID, nombre)
-VALUES ('CAT1', 'Electronics'),
-       ('CAT2', 'Clothing'),
-       ('CAT3', 'Furniture');
+VALUES ('CAT1', 'Electronics');
+
 
 -- Inserting records into INV_Marcas table
 INSERT INTO INV_Marcas (marcaID, nombre)
-VALUES ('M1', 'Sony'),
-       ('M2', 'Samsung'),
-       ('M3', 'Nike');
+VALUES ('M1', 'Sony');
+
 
 -- Inserting records into INV_Garantias table
 INSERT INTO INV_Garantias (garantiaID, descripcion)
-VALUES ('G1', '1 year warranty'),
-       ('G2', '2 years warranty'),
-       ('G3', 'No warranty');
+VALUES ('G1', '1 year warranty');
 
